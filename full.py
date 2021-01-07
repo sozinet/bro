54fb94bde2d8ba9d9fdbdb10631cfe70e781b7c0
sozinet <67073001+sozinet@users.noreply.github.com>
2021-01-07 18:30:30 +0300

    Update full.py

--- full.py
+++ full.py
@@ -1,5 +1,7 @@
 """
-Program
+Program do sterowania systemem automatyzacji hodowli roslin
+Andrew Sozinov
+Politechnika Bialostocka, 2021
 """
 import time
 import sys
@@ -12,10 +14,12 @@ from TSL2581 import TSL2581
 myAPI = "8XMCYIBW1SZCV10K"
 baseURL = 'http://api.thingspeak.com/update?api_key=%s' % myAPI
 
+#Definicja pinow
 DHTpin = 4
 LEDpin = 17
 FANpin = 27
 
+#Ustawienia pinow
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LEDpin, GPIO.OUT)
 GPIO.output(LEDpin, GPIO.LOW)
@@ -23,38 +27,42 @@ GPIO.setup(FANpin, GPIO.OUT)
 GPIO.output(FANpin, GPIO.LOW)
 GPIO.setup(DHTpin, GPIO.IN)
 
+#funkcja pobierania danych z czujnika temperatury i wilgotnosci
 def getSensorData():
     try:
         RHW, TW = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHTpin)
-   
-    # return dict
         return (str(RHW), str(TW))
     except:
         return ('error', 'error')
-    
+
+#funkcja pobierania danych z czujnika natenia owatlenia    
 def getTSLSensorData(Light):
     try:
         return str(Light.calculate_Lux(2, 148))
     except:
         return 'error'
+#start programu glownego
 print ('starting...')
 try:
     
     Light=TSL2581(0X39, debug=False)
 
     Light.Init_TSL2581()
-
     while True:
+#Uruchomienie funkcji do pobrania danych z czujnikow
       lux  =  getTSLSensorData(Light)
+      LT = int(lux)*4
+      lux4 = str(LT)
       RHW, TW = getSensorData()
+
+#przesylanie danych do servisu internetowego oraz ich wyswietlenie w terminalu
       f = urllib2.urlopen(baseURL + 
                                 "&field1=%s&field2=%s" % (TW, RHW)+
-                                "&field3=%s" % (lux))
-      print(TW + " " + RHW + " " +  lux)
+                                "&field3=%s" % (lux4))
+      print(TW + " " + RHW + " " +  lux4) 
       
 #zapalenie swiatla przy zbyt malym oswiatleniu
-      LT=int(lux)
-      if LT >= 10:
+      if LT >= 900:
         GPIO.output(LEDpin, GPIO.HIGH)
       else:
         GPIO.output(LEDpin, GPIO.LOW)
